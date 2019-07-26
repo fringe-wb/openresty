359840a4827884291e41a753d1a2994ac4f4352c
fringe-wb <52822597+fringe-wb@users.noreply.github.com>
2019-07-27 07:34:01 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+yum  groupinstall "Development tools" -y
+yum install -y pcre* gzip*  zlib* libxml* *xslt* gd gd-devel geoip geoip-devel 
+wget https://openresty.org/download/openresty-1.15.8.1.tar.gz
+tar zxvf openresty-1.15.8.1.tar.gz
+cd openresty-1.15.8.1
+./configure --prefix=/opt/OpenResty  --with-threads  --with-file-aio    --with-ipv6     --with-http_v2_module   --with-http_realip_module    --with-http_addition_module    --with-http_xslt_module   --with-http_image_filter_module    --with-http_geoip_module    --with-http_sub_module   --with-http_dav_module    --with-http_mp4_module   --with-http_dav_module    --with-http_gunzip_module  --with-http_auth_request_module    --with-http_random_index_module     --with-http_secure_link_module    --with-http_slice_module   --with-http_stub_status_module   --with-openssl=/root/openssl-1.1.0k
+gmake
+gmake install
